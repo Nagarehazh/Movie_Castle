6d2ef833a282aaef91b8f240bda60d005a81bef5
Nagarehazh <proyectosexitososya@gmail.com>
2022-09-15 21:09:11 -0500

    Gravatar Fix

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -86,7 +86,7 @@ function NavBar() {
                 <Avatar
                   style={{ width: 30, height: 30 }}
                   alt="Profile"
-                  src={`https://www.themoviedb.org/t/p/w64_and_h64_face${user?.avatar?.tmdb?.avatar_path}`}
+                  src={user?.avatar?.gravatar?.hash ? `https://www.gravatar.com/avatar/${user?.avatar?.gravatar?.hash}` : `https://www.themoviedb.org/t/p/w64_and_h64_face${user?.avatar?.tmdb?.avatar_path}`}
                 />
               </Button>
             )}
